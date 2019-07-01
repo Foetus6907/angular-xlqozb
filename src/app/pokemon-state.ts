3d372bbf0d119a1c4c86585cb4bcb85129b2b241
Foetus6907 <mghoubali@gmail.com>
2019-07-01 12:08:52 +0200

    6 files modified, 1 added, 1 deleted

--- src/app/pokemon-state.ts
+++ src/app/pokemon-state.ts
@@ -12,79 +12,84 @@ const TYPES = [
     iconUrl:"https://cdn.jsdelivr.net/gh/Foetus6907/angular-xlqozb@master/grass.png"
   },
   {
-    id:"1",
+    id:"2",
     name:"normal",
     iconUrl:"https://cdn.jsdelivr.net/gh/Foetus6907/angular-xlqozb@master/normal.png"
   },
   {
-    id:"1",
+    id:"3",
     name:"fire",
     iconUrl:"https://cdn.jsdelivr.net/gh/Foetus6907/angular-xlqozb@master/fire.png"
   },
   {
-    id:"1",
+    id:"4",
     name:"glass",
     iconUrl:"https://cdn.jsdelivr.net/gh/Foetus6907/angular-xlqozb@master/glass.png"
   },
   {
-    id:"1",
+    id:"5",
     name:"poison",
     iconUrl:"https://cdn.jsdelivr.net/gh/Foetus6907/angular-xlqozb@master/poison.png"
   },
   {
-    id:"1",
+    id:"6",
     name:"ground",
     iconUrl:"https://cdn.jsdelivr.net/gh/Foetus6907/angular-xlqozb@master/ground.png"
   },
   {
-    id:"1",
+    id:"7",
     name:"fight",
     iconUrl:"https://cdn.jsdelivr.net/gh/Foetus6907/angular-xlqozb@master/fight.png"
   },
   {
-    id:"1",
+    id:"7",
     name:"psy",
     iconUrl:"https://cdn.jsdelivr.net/gh/Foetus6907/angular-xlqozb@master/psy.png"
   },
   {
-    id:"1",
+    id:"8",
     name:"gosth",
     iconUrl:"https://cdn.jsdelivr.net/gh/Foetus6907/angular-xlqozb@master/gosth.png"
   },
   {
-    id:"1",
+    id:"9",
     name:"fairy",
     iconUrl:"https://cdn.jsdelivr.net/gh/Foetus6907/angular-xlqozb@master/fairy.png"
   },
   {
-    id:"1",
+    id:"10",
     name:"electric",
     iconUrl:"https://cdn.jsdelivr.net/gh/Foetus6907/angular-xlqozb@master/electric.png"
   },
   {
-    id:"1",
+    id:"11",
     name:"dragon",
     iconUrl:"https://cdn.jsdelivr.net/gh/Foetus6907/angular-xlqozb@master/dragon.png"
   },
   {
-    id:"1",
+    id:"12",
     name:"bug",
     iconUrl:"https://cdn.jsdelivr.net/gh/Foetus6907/angular-xlqozb@master/bug.png"
   },
   {
-    id:"1",
-    name:"flight",
+    id:"13",
+    name:"flying",
     iconUrl:"https://cdn.jsdelivr.net/gh/Foetus6907/angular-xlqozb@master/flight.png"
   },
   {
-    id:"1",
+    id:"14",
     name:"rock",
     iconUrl:"https://cdn.jsdelivr.net/gh/Foetus6907/angular-xlqozb@master/rock.png"
   },
   {
-    id:"1",
+    id:"15",
     name:"steel",
     iconUrl:"https://cdn.jsdelivr.net/gh/Foetus6907/angular-xlqozb@master/steel.png"
+  },
+  {
+    id:"16",
+    name:"water",
+    iconUrl:"https://cdn.jsdelivr.net/gh/Foetus6907/angular-xlqozb@master/water.png"
   },
 ];
 
--- src/app/type/type.component.ts
+++ src/app/type/type.component.ts
@@ -12,6 +12,7 @@ import { Type } from '../type';
 export class TypeComponent implements OnInit {
 
   @Input() types: [];
+  @Input() showLabel: boolean;
   currentTypes: Type[];
 
   constructor(private store: PokemonStore) { 
@@ -28,26 +29,16 @@ export class TypeComponent implements OnInit {
   }
 
   setType(){
-    console.log(this.types)
     this.currentTypes = new Array<Type>();
-    console.log(this.currentTypes)
-
-
-     for(var i = 0 ; i < this.types.length;i++)
-     {
+    for(var i = 0 ; i < this.types.length;i++)
+    {
       if(this.store.state.types.find(type=>type.name==this.types[i].type.name))
       {
-       this.currentTypes.push(this.store.state.types.find(type=>type.name==this.types[i].type.name));
-      } 
-      else {
+        this.currentTypes.push(this.store.state.types.find(type=>type.name==this.types[i].type.name));
+      } else {
         this.currentTypes.push(new Type());
       }
-      
-      
-     }
-    console.log(this.currentTypes)
-
-
+    }
   }
 
 }
